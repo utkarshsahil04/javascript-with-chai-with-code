98c44b3556997c7830086902d2f7e637f95cad2c
utkarshsahil04 <utkarsh.verma.cs28@iilm.edu>
2026-04-18 12:22:30 +0000

    till datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accoundID=12345  //no change 
+let acountEmail="sahil@gmail.com"
+var accountPassword="sahil@12"
+accountcity="varanasi"
+console.log(accountcity)
+console.table([accoundID,acountEmail,accountPassword,accountcity])
+
+/* 
+prefer not to use var
+because of in block scope and funcitonal scope
+
+*/
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict" //treat all js code as newer version 
+
+// alert() // we are using ndoejas , not browser 
+
+console.log(3+3)
+
+let name= "sahil"
+let age=20
+let isLoggedIn=false
+
+// number =>  2^52
+//bigint used in stoccks and othe5r with vvery large number 
+//string => ""
+//boolean => true / false
+// null 
+//undefined =>
+// symbol
+
+//object
+
+console.log(typeof null); // object ha 
+console.log(typeof undefined); // object ha 
